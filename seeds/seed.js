67e24654e6498d745cdbd42f133494dffa0cedea
coleloui <coleloui18@gmail.com>
2021-07-28 14:38:12 -0700

    fixing seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -7,8 +7,7 @@ const cardSeedData = require('./cardSeedData.json');
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  // const users =
-  await User.bulkCreate(userSeedData);
+  const users = await User.bulkCreate(userSeedData);
 
   for (const card of cardSeedData) {
     const newCard = await Card.create({
